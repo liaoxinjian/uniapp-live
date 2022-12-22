3779e8c40c22a01305866da0311b8a9546217d07
liaoxinjian <494461835@qq.com>
2022-12-22 17:45:45 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script>
 	export default {
 		onLaunch: function() {
-			console.log('App Launch')
+			console.log('App Launch', uni)
+			// const domModule = uni.requireNativePlugin('dom');
+			// domModule.addRule('fontFace', {
+			// 	'fontFamily': 'iconfont',
+			// 	'src': "url('https://at.alicdn.com/t/c/font_3836179_4fr6zttv627.ttf?t=1671695330737')"
+			// })
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -14,4 +19,8 @@
 
 <style>
 	/*每个页面公共css */
+	@import url("/common/css/common.css");
+	/* #ifndef */
+	@import url("/common/css/icon.css");
+	/* #endif */
 </style>
---
+++ src/pages/my/my.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
